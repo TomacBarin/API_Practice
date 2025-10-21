4732449d17bad82692f1ffe4359740d7e0f72bec
Tomac Barin Jansson <tomacbarin@me.com>
2025-10-21 22:50:53 +0200

    Added an input. User can now search for GIFs.

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const btnHtml = document.querySelector(".btn");
 const imgHtml = document.querySelector("img");
+const inputHtml = document.getElementById("input");
 
     function fetchNewCatGif() {
+      const searchTerm = inputHtml.value || "cats";
       fetch(
-        "https://api.giphy.com/v1/gifs/translate?api_key=lbTbjqF93pBniUzI9ZV23WcwcZq7Da7G&s=cats"
+        `https://api.giphy.com/v1/gifs/translate?api_key=lbTbjqF93pBniUzI9ZV23WcwcZq7Da7G&s=${searchTerm}`
       )
         .then(function (response) {
           return response.json();
         })
         .then(function (response) {
           imgHtml.src = response.data.images.original.url;
+        })
+        .catch(function (error) {
+          console.error("Whoops. Sorry:", error);
+          imgHtml.src = "#";
         });
     }
 
